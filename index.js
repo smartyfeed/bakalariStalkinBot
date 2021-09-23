3a473d6f749d199e5e28194aeba80fd26a1b144b
smartyfeed <7821902-smartyfeed@users.noreply.gitlab.com>
2021-09-23 10:29:28 +0200

    Presence added

--- index.js
+++ index.js
@@ -3,42 +3,62 @@ require('./bakalariStalkin/util/updateClassIDs.js');
 const stalk = require('./stalk.js');
 const fs = require('fs');
 const getTT = require('./bakalariStalkin/util/getClassTT.js');
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const {
+  Client,
+  Collection,
+  Intents
+} = require('discord.js');
+const {
+  token
+} = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS]
+});
 stalk.client = client;
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
 if (!fs.existsSync('./subscriptions.json')) {
-	fs.writeFileSync('./subscriptions.json', JSON.stringify({ subscriptions:[] }));
+  fs.writeFileSync('./subscriptions.json', JSON.stringify({
+    subscriptions: []
+  }));
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
-	stalk.closestNotification();
+  console.log('Ready!');
+  client.user.setPresence({
+    activities: [{
+      name: 'everyone',
+      type: 'WATCHING'
+    }],
+    status: 'online'
+  });
+  stalk.closestNotification();
 });
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    return interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true
+    });
+  }
 });
 
 
