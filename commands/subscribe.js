07c90c46ce59ea277ff07c3ea71b2d3847ba260b
smartyfeed <7821902-smartyfeed@users.noreply.gitlab.com>
2021-09-25 00:10:42 +0200

    Subscribe command + stuff

---
+++ commands/subscribe.js
@@ -0,0 +1,43 @@
+const generic = require('../bakalariStalkin/util/generic.js');
+const cli = require('cli');
+const cliui = require('cliui');
+const {
+  SlashCommandBuilder
+} = require('@discordjs/builders');
+const fs = require('fs');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('subscribe')
+    .setDescription('Initiate a stalking session for a specific class/group.')
+    .addStringOption(option => option.setName('class').setDescription('Class you want to subscribe to (for example P3.B)').setRequired(true))
+    .addStringOption(option => option.setName('groups').setDescription('Preferred groups (for example 1.sk or 1.sk 3.sk)').setRequired(false))
+    .addStringOption(option => option.setName('label').setDescription('Your "genius" label').setRequired(false)),
+  async execute(interaction) {
+    const className = interaction.options.getString('class').toUpperCase();
+    const groups = interaction.options.getString('groups')?.split(" ").filter(a => a) || [];
+    const label = interaction.options.getString('label') || className;
+
+    if (!generic.getClassInfo(className)) {
+      cli.error(`Incorrect class (${className}) entered by ${interaction.user.username} | ${interaction.user.id}`)
+      return interaction.reply(`Incorrect class: ${className}`);
+    }
+    for (var i = 0; i < groups.length; i++) {
+      if (!/^[1-4].sk$/.test(groups[i])) {
+        cli.error(`Incorrect group (${groups[i]}) entered by ${interaction.user.username} | ${interaction.user.id}`)
+        return interaction.reply(`Incorrect group: ${groups[i]}`);
+      }
+    }
+    var entry = {
+      userID: interaction.user.id,
+      className,
+      groups,
+      label,
+    };
+    var save = JSON.parse(fs.readFileSync("./subscriptions.json", "UTF8"));
+    save.subscriptions.push(entry);
+    fs.writeFileSync("./subscriptions.json", JSON.stringify(save, null, 2));
+    cli.ok(`${interaction.user.username} started stalking ${className} ${groups} | ID: ${interaction.user.id}`)
+    return interaction.reply(`Successfully started stalking! :sunglasses:`);
+  },
+};
