73c84870ea17340fd2cf5bf4f200285f62082a39
smartyfeed <kolya.fomichev@me.com>
2022-08-31 21:34:38 +0200

    Possible fix of /list api call

--- api/list.js
+++ api/list.js
@@ -1,8 +1,10 @@
 const db = require("../lib/dbpromise");
 const fetch = require('node-fetch');
+const generic = require('../bakalariStalkin/util/generic.js');
 
 module.exports = async function(req, res) {
   var { user } = req.session;
-  var activeSubs = await db.all("SELECT className, groups, pausedUntil, label FROM subscriptions WHERE userID = ?", user.id);
+  var activeSubs = await db.all("SELECT * FROM subscriptions WHERE userID = ?", user.id);
+  activeSubs.map(sub => {sub.className = generic.getClassInfo(sub.classID, false, sub.bakaServer).name;});
   res.json(activeSubs);
 }
