3a35c118f484e731fa2af7f44297830b5be306aa
smartyfeed <kolya.fomichev@me.com>
2023-04-24 01:18:53 +0200

    Sorting of active subs by label

--- api/list.js
+++ api/list.js
@@ -6,5 +6,7 @@ module.exports = async function(req, res) {
   var { user } = req.session;
   var activeSubs = await db.all("SELECT * FROM subscriptions WHERE userID = ?", user.id);
   activeSubs.map(sub => {sub.className = generic.getClassInfo(sub.classID, false, sub.bakaServer).name;});
+
+  activeSubs.sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0))
   res.json(activeSubs);
 }
